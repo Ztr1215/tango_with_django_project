a18c3cacd98d8422b061589afdccd133db851813
Tianrun Zhao <2672759Z@student.gla.ac.uk>
2022-02-09 10:46:08 +0000

    chapter6 completed with test

--- populate_rango.py
+++ populate_rango.py
@@ -8,27 +8,27 @@ from rango.models import Category, Page
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url': 'http://docs.python.org/3/tutorial/'},
+         'url': 'http://docs.python.org/3/tutorial/','views':2},
         {'title': 'How to Think like a Computer Scientist',
-         'url': 'http://www.greenteapress.com/thinkpython/'},
+         'url': 'http://www.greenteapress.com/thinkpython/','views':30},
         {'title': 'Learn Python in 10 Minutes',
-         'url': 'http://www.korokithakis.net/tutorials/python/'}
+         'url': 'http://www.korokithakis.net/tutorials/python/','views':100}
     ]
 
     django_pages = [
         {'title': 'Official Django Tutorial',
-         'url': 'https://docs.djangoproject.com/en/2.0/intro/tutorial01/'},
+         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/','views':64},
         {'title': 'Django Rocks',
-         'url': 'http://www.djangorocks.com'},
+         'url': 'http://www.djangorocks.com','views':15},
         {'title': 'How to Tango with Django',
-         'url': 'http://www.tangowithdjango.com'}
+         'url': 'http://www.tangowithdjango.com/','views':57}
     ]
 
     other_pages = [
         {'title': 'Bottle',
-         'url': 'http://bottlepy.org/docs/dev/'},
+         'url': 'http://bottlepy.org/docs/dev/','views':6},
         {'title': 'Flask',
-         'url': 'http://flask.pocoo.org'}
+         'url': 'http://flask.pocoo.org','views':25}
     ]
 
     cats = {'Python': {'pages': python_pages, 'views':128, 'likes':64},
@@ -38,7 +38,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, views = cat_data['views'],likes = cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'],views = p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
